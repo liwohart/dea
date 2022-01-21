f2d52d0b4629ff14b2f463c0186a07c4cf77ba1d
Leonardo Moraes <leomora@motorola.com>
2022-01-21 13:56:10 -0400

    Flag system improviment for

--- primal_dea.py
+++ primal_dea.py
@@ -27,18 +27,56 @@ def make_ppl(dmu, dt, inputs, outputs, v, u):
 
     return temp_ppl
 
-if __name__ == '__main__':
-    assert len(argv) > 4, 'insuficient arguments'
+def get_inputs(argv):
+    assert '-i' in argv or '--inputs' in argv, "no inputs provided."
+    try:
+        flag_pos = argv.index('-i')
+    except:
+        flag_pos = argv.index('--inputs')
+    end = flag_pos + 1
+    while end < len(argv) and not argv[end].startswith('-'):
+        end += 1
+    assert end != flag_pos + 1, "no inputs provided."
+    return argv[flag_pos + 1 : end]
+
+
+def get_outputs(argv):
+    assert '-o' in argv or '--outputs' in argv, "no outputs provided."
+    try:
+        flag_pos = argv.index('-o')
+    except:
+        flag_pos = argv.index('--outputs')
+    end = flag_pos + 1
+    while end < len(argv) and not argv[end].startswith('-'):
+        end += 1
+    assert end != flag_pos + 1, "no outputs provided."
+    return argv[flag_pos + 1 : end]
+
+def get_dmu(argv):
+    if '-d' in argv or '--dmu' in argv:
+        try:
+            flag_pos = argv.index('-d')
+        except:
+            flag_pos = argv.index('--dmu')
+        assert flag_pos + 1 < len(argv) and not argv[flag_pos + 1].startswith('-'), "no `dmu` specified."
+        dmu = argv[flag_pos + 1]
+    else:
+        dmu = 'dmu'
+    return dmu
+
+def parse_arguments():
     file_path = argv[1]
-    name = file_path[file_path.rindex(os.sep)+1:file_path.rindex('.')]
+    name = file_path[file_path.rindex(os.sep) + 1 : file_path.rindex('.')]
+    dmu = get_dmu(argv)
+    inputs = get_inputs(argv)
+    outputs = get_outputs(argv)
+    return file_path, name, dmu, inputs, outputs
 
-    dt = pd.read_csv(file_path).set_index('dmu')
-    n_inputs, n_outputs = int(argv[2]), int(argv[3])
-    assert len(argv) >= 4 + n_inputs + n_outputs, 'insuficient variables'
 
-    inputs = argv[4:4+n_inputs]
-    outputs = argv[4+n_inputs:4+n_inputs+n_outputs]
+if __name__ == '__main__':
+    file_path, name, dmu, inputs, outputs = parse_arguments()
 
+    dt = pd.read_csv(file_path).set_index(dmu)
     v = [f'peso_{i}' for i in inputs]
     u = [f'peso_{o}' for o in outputs]
 
@@ -72,14 +110,13 @@ if __name__ == '__main__':
 
     results = dt[inputs+outputs+v+u+razao]
 
-    if argv[-1].isnumeric() and bool(int(argv[-1])):
-        results_path = os.path.join('results',f'{name}.xlsx')
-
-        with pd.ExcelWriter(results_path) as writer:
-            print(f'loading {results_path}...')
-            results.to_excel(writer,sheet_name='main_results')
-            rests.to_excel(writer,sheet_name='env_map')
+    results_path = os.path.join('results',f'{name}.xlsx')
 
     print(results)
     print(rests)
+
+    with pd.ExcelWriter(results_path) as writer:
+        print(f'loading {results_path}...')
+        results.to_excel(writer,sheet_name='main_results')
+        rests.to_excel(writer,sheet_name='env_map')
 
